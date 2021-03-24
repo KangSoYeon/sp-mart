bc1f45778af6f8b4f1e6bcf41db304d65d1edda7
KangSoYeon <aaa0311@naver.com>
2021-03-24 20:41:25 +0900

    firebase setting

---
+++ src/components/base.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
+import "firebase/database";
+import "firebase/analytics";
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+export default app;
+firebase.analytics();
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import app from '../components/base'
 
 const Main = () => {
+
+    
     return (
         <>
             hi
