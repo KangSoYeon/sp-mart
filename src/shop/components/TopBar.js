3d5a7e3dd8bd7cd70a5d43d4fb244076ad066424
soyeon.kang <soyeon.kang@lge.com>
2021-04-06 18:30:21 +0900

    add Header Temporary

--- src/shop/components/TopBar.js
+++ src/shop/components/TopBar.js
@@ -31,7 +31,6 @@ const TopBar = () => {
         })
     }
 
-
     return (
         <>
             { mode === "guest" && <>
--- src/shop/pages/Main.js
+++ src/shop/pages/Main.js
@@ -4,11 +4,13 @@ import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import FooterInfo from '../components/FooterInfo';
 import TopBar from '../components/TopBar';
+import Header from '../components/Header';
 
 
 const Main = () => {
     return (
         <div>
+            <Header />
             <TopBar />
             <NavBar />
             요기 제목~
