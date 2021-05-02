e549b6435bbffd9756bd3d459e961709094e6de0
KangSoYeon <aaa0311@naver.com>
2021-05-02 15:56:44 +0900

    change slider

--- src/shop/pages/MainTitle.js
+++ src/shop/pages/MainTitle.js
@@ -12,18 +12,28 @@ const MainTitle = () => {
 
     return (
         <>
-            <SimpleImageSlider
+            <div className="slider">
+                <div className="figure">
+                    
+                    <img src={images[0].url} alt></img>
+                    <img src={images[1].url} alt></img>
+                    <img src={images[2].url} alt></img>
+                </div>
+            </div>
+            {/* <SimpleImageSlider
                 width={896}
                 height={504}
                 images={images}
                 showBullets={true}
                 showNavs={true}
+                slideDuration={0.5}
                 useGPURender={true}
                 navStyle={1}
                 navSize={50}
                 navMargin={30}
                 slideDuration={0.5}
-            />
+                style={{verticalAlign: "middle"}}
+            /> */}
             <Typography>Top Rate</Typography>
             물품 여덟개 정도
         </>
